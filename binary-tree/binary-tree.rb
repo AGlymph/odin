346b2af898a3b710ebbb5411c39cc14b17251f2b
AGlymph <64643785+AGlymph@users.noreply.github.com>
2025-08-29 15:41:46 +0900

    binary search day 1

--- binary-tree/binary-tree.rb
+++ binary-tree/binary-tree.rb
@@ -1,11 +1,11 @@
 class Node
   include Comparable
-  attr_accessor :data, :left_child, :right_child
+  attr_accessor :data, :left, :right
 
-  def initialize(data = nil, left_child = nil, right_child =nil)
+  def initialize(data = nil)
     @data = data
-    @left_child = left_child
-    @right_child = right_child
+    @left= nil
+    @right = nil
   end
 
   def <=> (other)
@@ -14,8 +14,63 @@ class Node
 
 end
 
-n1 = Node.new(1)
-n2 = Node.new(2)
+class Tree
+  
+  def initialize(array = nil)
+    array = array.uniq
+    array.sort!
+    @root = build_tree(array)
+  end
 
-p n1 > n2
-p n2 > n1
+  def build_tree(array)
+    return nil if array.empty?
+
+    mid_index = (array.length-1) / 2
+    node = Node.new(array[mid_index])
+    node.left = build_tree(array[0...mid_index])
+    node.right = build_tree(array [mid_index + 1...array.length])
+
+    return node
+  end
+
+  def insert(node = @root, value)
+    return Node.new(value) if node.nil? 
+    return node if value == node.data 
+
+    if value < node.data 
+      node.left = insert(node.left, value)
+    else 
+      node.right = insert(node.right,value)
+    end
+
+    return node
+  end
+
+  def delete (node = @root, value)
+    return nil if node.nil?
+
+    if value < node.data 
+      node.left = delete(node.left, value)
+    elsif value > node.data
+      node.right = delete(node.right,value)
+    else 
+      node.right = node 
+      #todo finish
+    end
+
+    return node 
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+
+end
+
+tree = Tree.new([2,4,3,5,6])
+tree.pretty_print
+tree.insert(1)
+tree.pretty_print
+
